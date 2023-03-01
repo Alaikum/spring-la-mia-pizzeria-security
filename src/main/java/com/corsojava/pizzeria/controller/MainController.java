5a0577115ecf1cdf0afde06edb1d448e0c8f6823
Alaikum <carangiadamo@gmail.com>
2023-03-01 16:26:22 +0100

    auth

--- src/main/java/com/corsojava/pizzeria/controller/MainController.java
+++ src/main/java/com/corsojava/pizzeria/controller/MainController.java
@@ -1,5 +1,6 @@
 package com.corsojava.pizzeria.controller;
 
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +11,15 @@ public class MainController {
 
 	
 	
-	//REDIRECT 
+	
 	@GetMapping
 	public String index() {
-		return "redirect:/pizze";
+		return "/index";
+	}
+	
+	@GetMapping("/testTLS")
+	public String testTLS(Authentication auth) {
+		System.out.println("Utente loggato:" +auth.name());
+		return "testTLS";
 	}
 }
